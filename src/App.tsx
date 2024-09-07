60f51563b11f21b17c551fc162d04ce98983752c
Le Anh Duc <ducvui2003@gmail.com>
2024-09-07 11:33:03 +0700

    test:pre-commit

--- src/App.tsx
+++ src/App.tsx
@@ -26,8 +26,7 @@ function App() {
 					count is {count}
 				</button>
 				<p>
-					Edit <code>src/App.tsxasdsadasdsadasdsad</code> and save to
-					test HMR
+					Edit <code>src/App</code> and save to test HMR
 				</p>
 			</div>
 			<p className="read-the-docs">
